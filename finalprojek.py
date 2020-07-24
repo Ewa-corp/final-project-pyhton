e076567abd649bd263e3c97ec26a898779a4cdc1
Ewa-corp <68749494+Ewa-corp@users.noreply.github.com>
2020-07-25 00:19:15 +0700

    Add files via upload
    Tugas Final project pyhton Teknik Komputer 1 2019
    Universitas Amikom Yogyakarta

---
+++ finalprojek.py
@@ -0,0 +1,177 @@
+
+print("__________kuis kenegaraan__________")
+print("===================================")
+print()
+print("**********************************")
+print("SOAL1")
+print("Rumusan Pancasila yang sah dan benar adalah yang terdapat dalam Pembukaan UUD 1945 dan disahkan oleh")
+print("a. BPUPKI")
+print("b. PPKI")
+print("c. KNPI")
+      
+jawaban = input()
+if jawaban == "b":
+    print("BENAR")
+else:
+    print("SALAH")
+print()
+print("*********************************")
+print("SOAL2")
+print("Nilai-nilai yang tekandung dalam Pancasila merupakan suatu sistem, khusus pada nilai yang terkandung dalam sila kedua ialah ")
+print("a. takwa kepada Tuhan Yang Maha Esa")
+print("b. demokratis")
+print("c. menjunjung tinggi HAM")
+
+jawaban = input()
+if jawaban == "c":
+    print("BENAR")
+else:
+    print("SALAH")
+print()
+print("*********************************")
+print("SOAL3")
+print("Nilai yang merupakan eksplisit, penjabaran lebih lanjut dari nilai-nilai dasar dalam rangka pelaksanaan nilai-nilai dasar Pancasila adalah ")
+print("a. nilai dasar")
+print("b. nilai instrumen")
+print("c. nilai praktis")
+
+jawaban = input()
+if jawaban == "b":
+    print("BENAR")
+else:
+    print("SALAH")
+
+print()
+print("*********************************")
+print("SOAL4")
+print("Pancasila sebagai dasar negara memiliki makna")
+print("a. tata nilai budaya maju")
+print("b. jiwa bangsa Indonesia")
+print("c. fondasi yang melandasi bangsa Indonesia mengatur tata pemerintahan")
+
+jawaban = input()
+if jawaban == "c":
+   print("BENAR")
+else:
+   print("SALAH")
+
+print()
+print("*********************************")
+print("SOAL5")
+print("Pancasila sebagai ciri khas bangsa Indonesia mempunyai arti ")
+print("a. setiap warga negara harus mendapat perlakuan hukum yang sama")
+print("b. kesepakatan nasional yang harus dipelihara bangsa")
+print("c. dapat membedakan bangsa Indonesia dengan bangsa lain")
+
+jawaban = input()
+if jawaban == "c":
+   print("BENAR")
+else:
+   print("SALAH")
+
+print()
+print("*********************************")
+print("SOAL6")
+print("Pentingnya ideologi bagi suatu bangsa adalah")
+print("a. keadaan yang menccerminkan pandangan bersama")
+print("b. sebagai dasar bagi berdirinya negara")
+print("c. tidak mudah terombang-ambing dalam menentukan tujuan nasional")
+
+jawaban = input()
+if jawaban == "c":
+   print("BENAR")
+else:
+   print("SALAH")
+
+print()
+print("*********************************")
+print("SOAL7")
+print("Pancasila dapat mempersatukan bangsa Indonesia dan memberi petunjuk dalam mencapai kesejahteraan dan kebahagiaan lahir dan batin bagi masyarakat Indonesia yang beraneka ragam sifatnya. Dari pernyataan tersebut, Pancasila berfungsi sebagai")
+print("a. pandangan hidup bangsa Indonesia")
+print("b. perjanjian luhur")
+print("c. kepribadian bangsa")
+
+jawaban = input()
+if jawaban == "a":
+   print("BENAR")
+else:
+   print("SALAH")
+
+print()
+print("*********************************")
+print("SOAL8")
+print("Pancasila sebagai ideologi terbuka, nilai-nilainya telah hidup dan berkembang sejak ")
+print("a. 17 Agustus 1945")
+print("b. sidang I BPUPKI")
+print("c. manusia Indonesia ada")
+
+jawaban = input()
+if jawaban == "c":
+   print("BENAR")
+else:
+   print("SALAH")
+   
+print()
+print("*********************************")
+print("SOAL9")
+print("Nilai-nilai yang terkandung dalam Pancasila sebagai ideologi terbuka adalah")
+print("a. nilai dasar, nilai materialis, nilai rohaniah")
+print("b. nilai dasar, nilai instrumen, nilai praksis")
+print("c. nilai dasar, nilai instrumen, nilai rohaniah")
+
+jawaban = input()
+if jawaban == "b":
+   print("BENAR")
+else:
+   print("SALAH")
+
+print()
+print("*********************************")
+print("SOAL10")
+print("Kumpulan gagasan, ide-ide, dan keyakinan yang menyeluruh dan sistematis dan mengatur tingkah laku manusia dalam berbagai kehidupan merupakan pencerminan dari")
+print("a. politik")
+print("b. strategi")
+print("c. ideologi")
+
+jawaban = input()
+if jawaban == "c":
+    print("BENAR")
+else:
+    print("SALAH")
+
+
+print("terimakasih")
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
